db08dfb52e1cae683b51fbe29ea0eed501edbabe
pauloALuis <50427097+pauloALuis@users.noreply.github.com>
2022-01-23 19:40:52 +0000

    added comments to all files

--- src/Comment.java
+++ src/Comment.java
@@ -1,5 +1,8 @@
 import java.util.Date;
 
+/**
+ * The type Comment.
+ */
 public class Comment {
 
     private Date dateComment = new Date();
@@ -7,6 +10,13 @@ public class Comment {
     private String textComment = "";
     private int likesComment =0;
 
+    /**
+     * Instantiates a new Comment.
+     *
+     * @param dateComment  the date comment
+     * @param textComment  the text comment
+     * @param likesComment the likes comment
+     */
     public Comment(Date dateComment, String textComment, int likesComment) {
         this.dateComment = dateComment;
         this.textComment = textComment;
@@ -22,31 +32,66 @@ public class Comment {
                 '}';
     }
 
+    /**
+     * Gets date comment.
+     *
+     * @return the date comment
+     */
     public Date getDateComment() {
         return dateComment;
     }
 
+    /**
+     * Sets date comment.
+     *
+     * @param dateComment the date comment
+     */
     public void setDateComment(Date dateComment) {
         this.dateComment = dateComment;
     }
 
+    /**
+     * Gets text comment.
+     *
+     * @return the text comment
+     */
     public String getTextComment() {
         return textComment;
     }
 
+    /**
+     * Sets text comment.
+     *
+     * @param textComment the text comment
+     */
     public void setTextComment(String textComment) {
         this.textComment = textComment;
     }
 
+    /**
+     * Gets likes comment.
+     *
+     * @return the likes comment
+     */
     public int getLikesComment() {
         return likesComment;
     }
 
+    /**
+     * Sets likes comment.
+     *
+     * @param likesComment the likes comment
+     */
     public void setLikesComment(int likesComment) {
         this.likesComment = likesComment;
     }
 
 
+    /**
+     * Get comment string.
+     *
+     * @return the string
+     */
     public String getComment (){
         return  "Comment:" + this.textComment
             +"\n\t Date: " +this.dateComment
--- src/Post.java
+++ src/Post.java
@@ -2,7 +2,10 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
- class Post {
+/**
+ * The type Post.
+ */
+class Post {
 
     private Date datePost;
     private String title;
@@ -11,47 +14,105 @@ import java.util.List;
     private List<Comment>  listComments ;
 
 
-     public Date getDatePost() {
+    /**
+     * Gets date post.
+     *
+     * @return the date post
+     */
+    public Date getDatePost() {
          return datePost;
      }
 
-     public void setDatePost(Date datePost) {
+    /**
+     * Sets date post.
+     *
+     * @param datePost the date post
+     */
+    public void setDatePost(Date datePost) {
          this.datePost = datePost;
      }
 
-     public String getTitle() {
+    /**
+     * Gets title.
+     *
+     * @return the title
+     */
+    public String getTitle() {
          return title;
      }
 
-     public void setTitle(String title) {
+    /**
+     * Sets title.
+     *
+     * @param title the title
+     */
+    public void setTitle(String title) {
          this.title = title;
      }
 
-     public String getContent() {
+    /**
+     * Gets content.
+     *
+     * @return the content
+     */
+    public String getContent() {
          return content;
      }
 
-     public void setContent(String content) {
+    /**
+     * Sets content.
+     *
+     * @param content the content
+     */
+    public void setContent(String content) {
          this.content = content;
      }
 
-     public int getLikes() {
+    /**
+     * Gets likes.
+     *
+     * @return the likes
+     */
+    public int getLikes() {
          return likes;
      }
 
-     public void setLikes(int likes) {
+    /**
+     * Sets likes.
+     *
+     * @param likes the likes
+     */
+    public void setLikes(int likes) {
          this.likes = likes;
      }
 
-     public List<Comment> getListComments() {
+    /**
+     * Gets list comments.
+     *
+     * @return the list comments
+     */
+    public List<Comment> getListComments() {
          return listComments;
      }
 
-     public void setListComments(Comment listComments) {
+    /**
+     * Sets list comments.
+     *
+     * @param listComments the list comments
+     */
+    public void setListComments(Comment listComments) {
          this.listComments.add(listComments)  ;
      }
 
-     public Post(Date datePost, String title, String content, int likes) {
+    /**
+     * Instantiates a new Post.
+     *
+     * @param datePost the date post
+     * @param title    the title
+     * @param content  the content
+     * @param likes    the likes
+     */
+    public Post(Date datePost, String title, String content, int likes) {
         this.datePost = datePost;
         this.title = title;
         this.content = content;
@@ -59,7 +120,11 @@ import java.util.List;
         this.listComments = new ArrayList<Comment>();
     }
 
-    //show all comments association in Post
+    /**
+     * Show post with comment string. And
+     *    show all comments association in Post
+     * @return the string
+     */
     public String showPostWithComment(){
         var comment = "";
         for (var item:this.listComments   ) {
@@ -79,7 +144,6 @@ import java.util.List;
                  '}';
      }
 
-     // print post
      private String showPost() {
          return "Post: \n "
                  +" Title: " + this.title
